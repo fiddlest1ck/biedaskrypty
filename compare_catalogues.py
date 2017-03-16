233b2c2fc3a85fd415e3b53900f3fd632447f0f3
maciej.kuzma <maciej.kuzma@cloudware.pl>
2017-03-16 06:36:58 +0100

    Frankenstein v2

--- compare_catalogues.py
+++ compare_catalogues.py
@@ -13,24 +13,39 @@ def new_path(new_package_path, path_for_create, x , files_list, first_item, othe
     path = [files_list.append(src_cp_tuple(new_package_path, path_for_create, item)) for item in first_item]
     return path
 
-path_a = '/home/fiddlest1ck/cat1'
-path_b = '/home/fiddlest1ck/cat2'
+path_a = '/home/fiddlest1ck/old_catalog'
+path_b = '/home/fiddlest1ck/new_catalog'
 path_create = '/home/fiddlest1ck/tmp_cat'
-files = []
-if True:
-    for x in ['dokumenty', 'sprawy', 'metadane']:
-        new_files_path = '{}/{}'.format(path_a, x)
-        old_files_path = '{}/{}'.format(path_b, x)
-        path_create1 = '{}/{}'.format(path_create, x)
-        if not os.path.exists(path_create):
-            os.mkdir(path_create)
-            os.mkdir(path_create1)
-        if os.listdir(new_files_path):
-            dircomp = filecmp.dircmp(new_files_path, old_files_path)
-            for function in [dircomp.left_only, dircomp.diff_files, dircomp.right_list]:
-                if function == dircomp.right_list:
-                    new_path(new_files_path, path_create1, x, files, function, dircomp.diff_files)
-                new_path(new_files_path, path_create1, x, files, function)
-    print(set(files))
-    for z, g in set(files):
-        copyfile(z, g)
+# files = []
+# if True:
+#     for x in ['dokumenty', 'sprawy', 'metadane']:
+#         new_files_path = '{}/{}'.format(path_a, x)
+#         old_files_path = '{}/{}'.format(path_b, x)
+#         path_create1 = '{}/{}'.format(path_create, x)
+#         if os.listdir(new_files_path):
+#             dircomp = filecmp.dircmp(new_files_path, old_files_path)
+#             for function in [dircomp.left_only, dircomp.diff_files, dircomp.right_list]:
+#                 if function == dircomp.right_list:
+#                     new_path(new_files_path, path_create1, x, files, function, dircomp.diff_files)
+#                 new_path(new_files_path, path_create1, x, files, function)
+#     print(set(files))
+#     for z, g in set(files):
+#         copyfile(z, g)
+
+deleted_files = []
+new_files = []
+delete_modified_files = []
+modified_files = []
+for x in ['dokumenty', 'metadane']:
+    old_files_path = '{}/{}'.format(path_a, x)
+    new_files_path = '{}/{}'.format(path_b, x)
+    dircomp = filecmp.dircmp(old_files_path, new_files_path)
+    print('{}/{}'.format(new_files_path, ''.join(dircomp.right_only)))
+    if dircomp.left_only:
+        deleted_files.append('{}/{}'.format(old_files_path, ''.join(dircomp.left_only)))
+    if dircomp.right_only:
+        new_files.append('{}/{}'.format(new_files_path, ''.join(dircomp.right_only)))
+    if dircomp.diff_files:
+        delete_modified_files.append('{}/{}'.format(old_files_path, ''.join(dircomp.diff_files)))
+        modified_files.append('{}/{}'.format(new_files_path, ''.join(dircomp.diff_files)))
+print({'deleted_files': deleted_files, 'new_files': new_files, 'modified_files': modified_files})
